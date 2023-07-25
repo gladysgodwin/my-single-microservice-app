bf53c9a11b96331a34467230c4a168df494f9fd6
gladysgodwin <gladysgodwin79@gmail.com>
2023-07-25 14:23:14 +0100

    started

--- webapp/app.js
+++ webapp/app.js
@@ -7,8 +7,6 @@ const bodyParser = require('body-parser');
 
 // MySQL database configuration
 const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE
